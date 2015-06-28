769885584a8be4b91f7df920fb9887bf351b694c
Sami Lamti <sami.lamti@gmail.com>
2015-06-28 14:02:20 +0200

    Blinky
    Inspired by http://start.tessel.io/blinky

---
+++ intro.js
@@ -0,0 +1,13 @@
+var tessel = require('tessel-fakes');
+var led1 = tessel.led[0].output(1);
+var led2 = tessel.led[1].output(0);
+var led3 = tessel.led[2].output(1);
+var led4 = tessel.led[3].output(0);
+
+setInterval(function() {
+	console.log("I'm blinking! (Press CTRL+C to stop)");
+	led1.toggle();
+	led2.toggle();
+	led3.toggle();
+	led4.toggle();
+}, 250);
